6e278b3b793eaa591c38b79e1dd1a785e156e306
Pablo Herranz <pablohr88@gmail.com>
2018-03-06 12:58:27 +0100

    Agregadas todas las cenas. Completada la generación del menú.

--- randFood.py
+++ randFood.py
@@ -5,6 +5,8 @@ import random
 import calendar
 import json
 random.seed(int(time.time() * 1000))
+
+weekDays = 5
 comidas = json.load(open("comidas.json", encoding='utf-8'))
 # cenas=json.load(open("cenas.json", encoding='utf-8'))
 # print(comidas)
@@ -16,19 +18,20 @@ comidas = json.load(open("comidas.json", encoding='utf-8'))
 # semanaComida = list(random.sample(range(0,len(comidas["comidas"])), 5))
 # semanaCena = list(random.sample(range(0,len(cenas["cenas"])), 5))
 
-randComidas = list(random.sample(range(0, len(comidas["comidas"])), 5))
+randComidas = list(random.sample(range(0, len(comidas["comidas"])), weekDays))
 semanaComidas = []
+randCenas = list(random.sample(range(0, len(comidas["cenas"])), weekDays))
+semanaCenas = []
 # randCenas = list(random.sample(range(0,len(cenas["cenas"])), 5))
 
-for i in range(5):
-    semanaComidas[i] = list(comidas["comidas"][randComidas[i]]["nombre"])
+for i in range(weekDays):
+    semanaComidas.append(comidas["comidas"][randComidas[i]]["nombre"])
+    semanaCenas.append(comidas["cenas"][randCenas[i]]["nombre"])
 
 print('<!DOCTYPE html>\n<html>\n<head>\n\t<link rel="stylesheet" href="style.css">\n\t<meta charset="UTF-8">\n</head>\n<body>')
-print("<table>")
-print('<tr id="cabecera"><th>Día</th><th>Comida</th><th>Cena</th></tr>')
-for i in range(5):
-    print('<tr id="cuerpo"><th>' +
-          calendar.day_name[i] + '</th><th>' + semanaComidas[i] + '</th></tr>')
-    # print('<tr id="cuerpo"><th>'+calendar.day_name[i]+'</th><th>'+cenas["cenas"][semanaCenas[i]]["nombre"]+'</th></tr>')
-print('</table>')
+print("\t<table>")
+print('\t\t<tr id="cabecera"><th>Día</th><th>Comida</th><th>Cena</th></tr>')
+for i in range(weekDays):
+    print('\t\t<tr id="cuerpo"><th>' + calendar.day_name[i] + '</th><th>' + semanaComidas[i] + '</th><th>' + semanaCenas[i] + '</th></tr>')
+print('\t</table>')
 print('</body>\n</html>')
