1b661df849d211360451dc69ad84a3f36acb27fe
Pablo Herranz Ramírez <pablohr88@gmail.com>
2018-03-11 21:44:22 +0000

    Formato CSS añadido al email

--- randFood.py
+++ randFood.py
@@ -31,7 +31,7 @@ for i in range(weekDays):
 
 print('From: pablohr88@gmail.com\nMIME-Version: 1.0\nContent-Type: text/html\nSubject: Prueba de menú\n\n')
 
-print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n<html>\n<head>\n\t<link rel="stylesheet" href="style.css">\n\t<meta charset="UTF-8">\n</head>\n<body>')
+print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n<html>\n<head>\n\t<link rel="stylesheet" href="style.css">\n\t<meta charset="UTF-8">\n\t<style>\n\ttable, th, td {border-style: solid; border-width: 2px}\n\t#cabecera{font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif; text-transform: uppercase; color: darkslategray; text-align: center}\n\t#cuerpo{font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif; font-weight: 50; text-align: left}\n\t</style></head>\n<body>')
 print("\t<table>")
 print('\t\t<tr id="cabecera"><th>Día</th><th>Comida</th><th>Cena</th></tr>')
 for i in range(weekDays):
