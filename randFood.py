3993bbe88a74754dc90ca60b64d970509dbd04c3
Pablo Herranz <pablohr88@gmail.com>
2020-08-30 20:36:18 +0200

    Modificado randFood.py para que escriba a menu.html. Cambiados los formatos a Windows-1252.

--- randFood.py
+++ randFood.py
@@ -1,14 +1,17 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
+# -*- coding: windows-1252 -*-
+
 import time
 import random
 import calendar
 import json
 import collections
+import sys
+
 random.seed(int(time.time() * 1000))
 ingredientes = []
 weekDays = 5
-comidas = json.load(open("comidas.json", encoding='utf-8'))
+comidas = json.load(open("comidas.json", encoding='windows-1252'))
 
 randComidas = list(random.sample(range(0, len(comidas["comidas"])), weekDays))
 semanaComidas = []
@@ -19,30 +22,42 @@ for i in range(weekDays):
     semanaComidas.append(comidas["comidas"][randComidas[i]])
     semanaCenas.append(comidas["cenas"][randCenas[i]])
 
-print('From: pablohr88@gmail.com\nMIME-Version: 1.0\nContent-Type: text/html\nSubject: Men√∫ semanal <3\n\n')
+original_stdout = sys.stdout
+with open("menu.html", "w", encoding='windows-1252') as file:
+    sys.stdout = file
+    print('From: pablohr88@gmail.com\nMIME-Version: 1.0\nContent-Type: text/html\n\
+           Subject: Men˙ semanal <3\n\n')
 
-print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">\n<html>\n<head>\n\t<link rel="stylesheet" href="style.css">\n\t<meta charset="UTF-8">\n\t<style>\n\ttable, th, td {border-style: solid; border-width: 2px}\n\t#cabecera{font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif; text-transform: uppercase; color: darkslategray; text-align: center}\n\t#cuerpo{font-family: "Palatino Linotype", "Book Antiqua", Palatino, serif; font-weight: 50; text-align: left}\n\t</style></head>\n<body>')
-print("\t<table>")
-print('\t\t<tr id="cabecera"><th>D√≠a</th><th>Comida</th><th>Cena</th></tr>')
-for i in range(weekDays):
-    print('\t\t<tr id="cuerpo"><th>' + calendar.day_name[i] + '</th><th>' + semanaComidas[i]["nombre"] + '</th><th>' + semanaCenas[i]["nombre"] + '</th></tr>')
-print('\t</table>')
+    print('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//ES" \
+        "http://www.w3.org/TR/html4/loose.dtd">\n<html>\n<head>\n\t<link \
+        rel="stylesheet" href="style.css">\n\t<meta charset="WINDOWS-1252">\n\t<style>\
+        \n\ttable, th, td {border-style: solid; border-width: 2px}\n\t\
+        #cabecera{font-family: "Palatino Linotype", "Book Antiqua", Palatino, \
+        serif; text-transform: uppercase; color: darkslategray; text-align: \
+        center}\n\t#cuerpo{font-family: "Palatino Linotype", "Book Antiqua", \
+        Palatino, serif; font-weight: 50; text-align: left}\n\t</style></head>\
+        \n<body>')
+    print("\t<table>")
+    print('\t\t<tr id="cabecera"><th>DÌ≠a</th><th>Comida</th><th>Cena</th></tr>')
+    for i in range(weekDays):
+        print('\t\t<tr id="cuerpo"><th>' + calendar.day_name[i] + '</th><th>' +\
+            semanaComidas[i]["nombre"] + '</th><th>' + semanaCenas[i]["nombre"] +\
+            '</th></tr>')
+    print('\t</table>')
 
-for i in semanaComidas:
-    ingredientes.extend(i["ingredientes"])
+    for i in semanaComidas:
+        ingredientes.extend(i["ingredientes"])
 
-for i in semanaCenas:
-    ingredientes.extend(i["ingredientes"])
+    for i in semanaCenas:
+        ingredientes.extend(i["ingredientes"])
 
-ingredientes = collections.Counter(ingredientes)
+    ingredientes = collections.Counter(ingredientes)
 
-print("\t<br><br>***LISTA DE LA COMPRA***")
-print("\t<ul>")
-for ing, num in ingredientes.items():
-    print("\t\t<li>" + str(num) + "x " + ing + "</li>")
-print("\t</ul>")
+    print("\t<br><br>***LISTA DE LA COMPRA***")
+    print("\t<ul>")
+    for ing, num in ingredientes.items():
+        print("\t\t<li>" + str(num) + "x " + ing + "</li>")
+    print("\t</ul>")
 
-# salida = salida.replace('Counter({', '')
-# print(salida.replace('})', ''))
-
-print('</body>\n</html>')
+    print('</body>\n</html>')
+    sys.stdout = original_stdout
